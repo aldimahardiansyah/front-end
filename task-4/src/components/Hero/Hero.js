6107d7950b97e280708033d5407e2fdd0995a1cb
aldimahardiansyah <aldimdiansyah@gmail.com>
2022-04-21 15:28:29 +0700

    pertemuan 9

--- task-4/src/components/Hero/Hero.js
+++ task-4/src/components/Hero/Hero.js
@@ -1,19 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import styles from "./Hero.module.css";
 
 function Hero(){
+    // destruct movie state
+    const [movie, setMovie] = useState("");
+
+    useEffect(async() => {
+        // melakukan effect: fetch data movie(api)
+        const response = await fetch("https://www.omdbapi.com/?apikey=d7497266&i=tt3896198");
+
+        const data = await response.json();
+        setMovie(data);
+    }, []);
+
     return (
         <div className={styles.container}>
             <section className={styles.hero}>
                 <div className={styles.hero__left}>
-                    <h2 className={styles.hero__title}>Spiderman</h2>
-                    <h3 className={styles.hero__genre}>Thriller, Drama, Romance</h3>
-                    <p className={styles.hero__description}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores est recusandae vero, rem eaque ipsam praesentium quaerat maiores! Officia, in!
-                    </p>
+                    <h2 className={styles.hero__title}>{movie.Title}</h2>
+                    <h3 className={styles.hero__genre}>Genre: {movie.Genre}</h3>
+                    <p className={styles.hero__description}>{movie.Plot}</p>
                     <button className={styles.hero__button}>Watch</button>
                 </div>
                 <div className={styles.hero__right}>
-                    <img className={styles.hero__image} src="https://picsum.photos/600/400" alt="" />
+                    <img className={styles.hero__image} src={movie.Poster} alt="" />
                 </div>
             </section>
         </div>
